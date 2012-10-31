819870ff3e33d7611f812ea34805a7db6fb60f79
Wojtek Jurczyk <wojtek.jurczyk@gmail.com>
2012-10-31 23:07:28 +0100

    The diverative of a sum

---
+++ sym-diff.pl
@@ -0,0 +1,19 @@
+% diff(expr, var, dive)
+
+% constant
+diff(E, _, 0) :- number(E).
+
+% the same variable
+diff(E, V, 0) :- 
+	atom(E),
+	E \== V.
+	
+% other variables
+diff(E, V, 1) :- 
+	atom(E),
+	E == V.
+	
+% sum
+diff(E1 + E2, V, Ed1 + Ed2) :- 
+	diff(E1, V, Ed1),
+	diff(E2, V, Ed2).
