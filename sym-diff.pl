3cb0d909f02f40881d140215709ef4e99ae2ce30
Wojtek Jurczyk <wojtek.jurczyk@gmail.com>
2012-10-31 23:43:21 +0100

    Multiplication and division

--- sym-diff.pl
+++ sym-diff.pl
@@ -23,4 +23,19 @@ diff(E1 - E2, V, Ed1 - Ed2) :-
 	diff(E1, V, Ed1),
 	diff(E2, V, Ed2).
 	
-
+% (fg)' = f'g + fg'
+diff(E1 * E2, V, Ed1*E2 + E1*Ed2) :- 
+	diff(E1, V, Ed1),
+	diff(E2, V, Ed2).
+	
+% (f/g)' = (f'g - fg') / (g*g)
+diff(E1 / E2, V, (Ed1*E2 - E1*Ed2)/(E2*E2)) :- 
+	diff(E1, V, Ed1),
+	diff(E2, V, Ed2).
+	
+% (h(g))' = h'(g) * g'
+%diff(F, X, Hd(G) * Gd) :-
+%	functor(F, __, 2),
+%	arg(0, F, G),
+	
+% (f^g)' = f^(g-1)*(gf' + g'f logf) 
