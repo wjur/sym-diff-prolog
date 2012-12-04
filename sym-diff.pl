db57d17caff831ceb320a42be9e5fb445db92f7b
Wojtek Jurczyk <wojtek.jurczyk@gmail.com>
2012-12-04 16:47:25 +0100

    Works! Need optimalizations

--- sym-diff.pl
+++ sym-diff.pl
@@ -1,4 +1,4 @@
-% diff(expr, var, dive)
+% diff(expr, var, div)
 
 % constant
 diff(E, _, 0) :- number(E).
@@ -32,49 +32,28 @@ diff(E1 * E2, V, Ed1*E2 + E1*Ed2) :-
 diff(E1 / E2, V, (Ed1*E2 - E1*Ed2)/(E2*E2)) :- 
 	diff(E1, V, Ed1),
 	diff(E2, V, Ed2).
-	
-% (make_mul  (change_fun (first expr) (nth 1 expr)) (diff (nth 1 expr) var)))
-% (h(g))' = h'(g) * g'
-diff(F, X, Hd * Gd) :-
-    functor(F, H, 1),
-    arg(1, H, G),
-    
-    change_fun(H, Hd),
-    
-    diff(G, X, Gd).
-    %diff(H, X, Hd),
-    %functor(HdG, Hd, 1),
-    %arg(1,Hd,G).
-	
-    
 
+% pochodne funkcji zgodnie ze wzorem
+% (h(g))' = h'(g) * g'
+diff(sin(E), V, Ed*cos(E)) :-
+    diff(E,V, Ed).
+
+diff(cos(E), V, -Ed*sin(E)) :-
+    diff(E,V, Ed).
+	
+diff(tan(E), V, Ed*2/(2*cos(2*E)+1)) :-
+    diff(E,V, Ed).
+
+diff(exp(E), V, Ed*exp(E)) :-
+    diff(E,V, Ed).	
+
+diff(log(E), V, Ed/E) :-
+    diff(E,V, Ed).		
+	
 % (f^g)' = f^(g-1)*(gf' + g'f logf) 
 diff(F^G, V, F^(G-1)*(G*Fd + Gd*F*log(F))) :-
     diff(F, V, Fd),
     diff(G, V, Gd).
 
-% diff sin = cos
-%diff(sin(E), V, Ed*cos(E)) :-
-%    diff(E,V, Ed).
-    
-%diff(cos(E), V, -Ed*sin(E)) :-
-%    diff(E,V, Ed).
-    
-%diff(exp(E), V, Ed*exp(E)) :-
-%    diff(E,V, Ed).
-    
-%diff(log(E), V, 1/Ed) :-
-%    diff(E,V, Ed).
-
-%change_fun(log(E), _, 1/E) :-
-
-change_fun(sin(E), cos(E)). 
-
-diff2(F,X, Hp + Gp) :-
-    change_fun(F, Hp),
-    arg(1, F, G),
-    diff(G, X, Gp),
-    %functor(HpG, Hp, 1).
-    arg(1, HpG, G).
 
 
