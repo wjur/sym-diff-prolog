a42972e464d1d1438848b10fccd4e7c591ca3fc3
Wojciech Jurczyk <wojtek.jurczyk@gmail.com>
2012-12-04 12:52:51 +0100

    change_fun

--- sym-diff.pl
+++ sym-diff.pl
@@ -34,11 +34,16 @@ diff(E1 / E2, V, (Ed1*E2 - E1*Ed2)/(E2*E2)) :-
 	diff(E2, V, Ed2).
 	
 % (h(g))' = h'(g) * g'
-%diff(F, X, Hd * Gd) :-
-%	functor(F, H, 1),
-%	arg(1, H, G),
-%    diff(G, X, Gd),
-%    diff(H, X, Hd).
+diff(F, X, HdG * Gd) :-
+    functor(F, H, 1),
+    arg(1, H, G),
+    
+    changeFun(H, Hd),
+    
+    diff(G, X, Gd).
+    %diff(H, X, Hd),
+    %functor(HdG, Hd, 1),
+    %arg(1,Hd,G).
 	
 % (f^g)' = f^(g-1)*(gf' + g'f logf) 
 diff(F^G, V, F^(G-1)*(G*Fd + Gd*F*log(F))) :-
@@ -46,11 +51,18 @@ diff(F^G, V, F^(G-1)*(G*Fd + Gd*F*log(F))) :-
     diff(G, V, Gd).
 
 % diff sin = cos
-diff(sin(E), V, Ed*cos(E)) :-
-    diff(E,V, Ed).
+%diff(sin(E), V, Ed*cos(E)) :-
+%    diff(E,V, Ed).
     
-diff(cos(E), V, -Ed*sin(E)) :-
-    diff(E,V, Ed).
+%diff(cos(E), V, -Ed*sin(E)) :-
+%    diff(E,V, Ed).
     
-diff(exp(E), V, Ed*exp(E)) :-
-    diff(E,V, Ed).
+%diff(exp(E), V, Ed*exp(E)) :-
+%    diff(E,V, Ed).
+    
+%diff(log(E), V, 1/Ed) :-
+%    diff(E,V, Ed).
+
+%change_fun(log(E), _, 1/E) :-
+
+change_fun(sin(E), _, cos(E)). 
